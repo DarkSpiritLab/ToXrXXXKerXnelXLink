3c87c2b34beb0053bf2eeea3450d20aea6909ce8
silver <mr16841042@gmail.com>
2018-05-21 22:10:27 +0800

    tested

--- kernel.py
+++ kernel.py
@@ -1,81 +1,124 @@
-
-# coding: utf-8
-
-# In[1]:
-
+from collections import defaultdict
+# import mysql.connector
+import datetime
 
 import json
 
 
-# In[17]:
+query = ("select my_ip,next_ip,next_port,prev_circ_id,next_circ_id,direction,stream_id,is_origin from net "
+         "where time between %s and %s ")
 
 
-# (id,) my_ip,next_ip,next_port,prev_circ_id,next_circ_id,direction,stream_id,is_origin
+# config={}
+# with open("config") as f:
+#     t=f.readlines()
+#     config=json.loads(t)
+
+
+# id,my_ip,next_ip,next_port,prev_circ_id,next_circ_id,direction,stream_id,is_origin
 class link:
-    def __init__(self,prev_circ_id,next_circ_id,my_ip,next_ip,next_port,direction,is_origin=False,stream_id=0):
-        self.prev_circ_id=prev_circ_id
-        self.next_circ_id=next_circ_id
-        # self.id=id
-        self.my_ip=my_ip
-        self.next_ip=next_ip
-        self.next_port=next_port
-        self.direction=direction
-        self.stream_id=stream_id
-        self.is_origin=is_origin
+    def __init__(self, my_ip, next_ip, next_port, prev_circ_id, next_circ_id, direction, stream_id = 0,
+                 is_origin = False, id = 0):
+        self.prev_circ_id = prev_circ_id
+        self.next_circ_id = next_circ_id
+        self.id = id
+        self.my_ip = my_ip
+        self.next_ip = next_ip
+        self.next_port = next_port
+        self.direction = direction
+        self.stream_id = stream_id
+        self.is_origin = is_origin
 
-def readFromDB():
+    def __str__(self):
+        return str("my_ip:" + self.my_ip)
+
+
+def readFromDB(offset = 30):
     '''
     using my_ip as key
     using prev_circ as second key (accelerate search whether prev_circ is there)
-    
-    return dict() --{"my_ip":{"prev_circ_id":link}}
-    '''
-    #as test
-    relay=dict()
-    origin=dict()
-    a=link("12","34",1,"10.0.0.1","10.0.0.3",600,123,True)
-    b=link("34","56",2,"10.0.0.3","10.0.0.4",700,123)
-    origin[a.my_ip]={a.prev_circ_id:a}
-    relay[b.my_ip]={b.prev_circ_id:b}
-    
-    return origin,relay
 
-origins,relays=dict(),dict() #global var
+    type offset:int  (minute)
+    rtype dict() --{"my_ip":{"prev_circ_id":link}}
+    '''
+    relay = defaultdict(dict)
+    origin = defaultdict(dict)
+
+    #     time_start="(select date_sub( ( select now() ), interval %d minute ))"%offset
+    #     time_end="( select now() )"
+
+    #     cnx = mysql.connector.connect(**config)
+    #     cursor=cnx.cursor()
+
+    #     cursor.execute(query,(time_start,time_end))
+
+    #     for (my_ip,next_ip,next_port,prev_circ_id,next_circ_id,direction,stream_id,is_origin) in cursor:
+    #         t=link(my_ip,next_ip,next_port,prev_circ_id,next_circ_id,direction,stream_id,is_origin)
+    #         if(is_origin):
+    #             origin[t.my_ip][t.prev_circ_id]=t
+    #         else:
+    #             relay[t.my_ip][t.prev_circ_id]=t
+    # as test
+
+    a = link("10.0.0.1", "10.0.0.3", 600, "12", "34", 1, 123, True)
+    b = link("10.0.0.3", "10.0.0.4", 700, "34", "56", 2, 123)
+    c = link("10.0.0.4", "10.0.0.7", 999, "56", "11111", 3, 123)
+    origin[a.my_ip] = {a.prev_circ_id: a}
+    relay[b.my_ip] = {b.prev_circ_id: b}
+    relay[c.my_ip] = {c.prev_circ_id: c}
+    #     cnx.close()
+    return origin, relay
+
+
+origins, relays = defaultdict(dict), defaultdict(dict)  # global var
+
+
 #     return None
-def searchNext(p,relays):
-    # p:type link  --the previous one 
+def searchNext(p, relays):
+    # todo using direction to estimate whether this relay is right
+    # p:type link  --the previous one
     # relay:type dict {"my_ip":{"prev_circ_id":link}} --the relay dict
-    # return list()  --the list from p to end looks like [p1,[p2,[p3,None]]]
-    if(p.next_ip in relays):
-        nls=relays[p.next_ip]
-        print(nls)
-        if(p.next_circ_id in nls): # prev_relay.next_circ_id equals to the next one's prev_circ_id
-            t=searchNext(nls[p.next_circ_id],relays)
-            return [p,t]
+    # return list()  --the list from p to end looks like [p1,p2,p3]
+    if (p.next_ip in relays):
+        nls = relays[p.next_ip]
+        #         print(nls)
+        if (p.next_circ_id in nls):  # prev_relay.next_circ_id equals to the next one's prev_circ_id
+            t = searchNext(nls[p.next_circ_id], relays)
+            if (t is not None):
+                t.insert(0, p)
+                return t
+            else:
+                # as end one is nls[p.next_circ_id]
+                return [p,nls[p.next_circ_id]]
     return None
 
 
+def printFixLinks(fixlinks):
+    # type fixlinks:dict("origin_ip":list [[p1,p2,p3],[p1,p3,p4,p5]])
+    #     print(fixlinks)
+    for i in fixlinks:
+        for ii in fixlinks[i]:
+            for node in ii:
+                print(node.my_ip + "->")
+            print("end one:"+ii[-1].next_ip)
+
+
 def linkAll():
-    # return fixlinks:type dict("origin_ip":list [[p1,[p2,None]],[p1,[p3,[p4,None]]]])
-    fixlinks=dict()
-    origins,relays=readFromDB()
-    print(origins,relays)
-    for oStr in origins:# though all the origin #this is a p
-        o=origins[oStr]
-        for prev_circ_id in o: #all the next ip
-            p=o[prev_circ_id]# one origin link point
-            print(p.next_ip)
-            t=searchNext(p,relays)
-            if(t!=None):
-                if(oStr not in fixlinks):
-                    fixlinks[oStr]=list()
+    # rtype fixlinks: dict("origin_ip":list [[p1,p2,p3],[p1,p3,p4,p5]])
+    fixlinks = defaultdict(list)
+    origins, relays = readFromDB()
+    #     print(origins,relays)
+    for oStr in origins:  # though all the origin #this is a p
+        o = origins[oStr]
+        for prev_circ_id in o:  # all the next ip
+            p = o[prev_circ_id]  # one origin link point
+
+            t = searchNext(p, relays)
+            if (t != None):
                 fixlinks[oStr].append(t)
     return fixlinks
-#             if(next_ip not in relays): #not such relay
-#                 continue
-#             else:
-#                 p=relay[next_ip]
-#                 searchNext(p)
-if __name__=="__main__":
-    print(linkAll())
 
+
+if __name__ == "__main__":
+    a = linkAll()
+    printFixLinks(a)
